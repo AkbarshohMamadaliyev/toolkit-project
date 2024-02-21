022130af1227e655f679bf764d55365067122fb9
test-name <test@gmail.com>
2024-02-21 09:50:21 +0500

    first commit

---
+++ src/ui/Input.jsx
@@ -0,0 +1,16 @@
+function Input({ label, state, setState, type = "text" }) {
+  return (
+    <div className="form-floating">
+      <input
+        type={type}
+        className="form-control"
+        value={state}
+        onChange={e => setState(e.target.value)}
+        id="floatingInput"
+        placeholder={label} />
+      <label htmlFor="floatingInput">{label}</label>
+    </div>
+  )
+}
+
+export default Input
