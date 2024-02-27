745018568084ebd2d79a898756e4a7f4b5d12637
User <akbarshokhmamadaliev@gmail.com>
2024-02-27 22:20:14 +0500

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,55 @@
-import { Link } from 'react-router-dom'
-import { logo } from "../constants"
-import { useSelector } from 'react-redux'
+import { Link, useNavigate } from "react-router-dom";
+import { logo } from "../constants";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItem } from "../helpers/persistance-storage";
+import { logoutUser } from "../slice/auth";
 
 function Navbar() {
-  const { loggedIn, user } = useSelector(state => state.auth)
+  const { loggedIn, user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logoutUser());
+    removeItem("token");
+    navigate("/login");
+  };
 
   return (
     <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3">
-      <Link to={'/'}>
+      <Link to={"/"}>
         <img src={logo} alt="" />
       </Link>
 
       <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
         {loggedIn ? (
           <>
-            <p className='me-3 py-2 m-0  text-decoration-none'>{user.username}</p>
-            <button className='btn btn-outline-danger'>Logout</button>
+            <p className="me-3 py-2 m-0  text-decoration-none">
+              {user.username}
+            </p>
+            <button className="btn btn-outline-danger" onClick={logoutHandler}>
+              Logout
+            </button>
           </>
         ) : (
           <>
-            <Link className='me-3 py-2 link-body-emphasis text-decoration-none' to={'/login'}>
+            <Link
+              className="me-3 py-2 link-body-emphasis text-decoration-none"
+              to={"/login"}
+            >
               Login
             </Link>
-            <Link className='me-3 py-2 link-body-emphasis text-decoration-none' to={'/register'}>
+            <Link
+              className="me-3 py-2 link-body-emphasis text-decoration-none"
+              to={"/register"}
+            >
               Register
             </Link>
           </>
         )}
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/slice/auth.js
+++ src/slice/auth.js
@@ -1,32 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { setItem } from '../helpers/persistance-storage'
+import { createSlice } from "@reduxjs/toolkit";
+import { setItem } from "../helpers/persistance-storage";
 
 const initialState = {
   isLoading: false,
   loggedIn: false,
   error: null,
-  user: null
-}
+  user: null,
+};
 
 export const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
-    signUserStart: state => {
-      state.isLoading = true
+    signUserStart: (state) => {
+      state.isLoading = true;
     },
     signUserSuccess: (state, action) => {
-      state.loggedIn = true
-      state.isLoading = false
-      state.user = action.payload
-      setItem('token', action.payload.token)
+      state.loggedIn = true;
+      state.isLoading = false;
+      state.user = action.payload;
+      setItem("token", action.payload.token);
     },
     signUserFailure: (state, action) => {
-      state.isLoading = false
-      state.error = action.payload
-    }
-  }
-})
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      state.loggedIn = false;
+    },
+  },
+});
 
-export const { signUserStart, signUserSuccess, signUserFailure } = authSlice.actions
-export default authSlice.reducer
+export const { signUserStart, signUserSuccess, signUserFailure, logoutUser } =
+  authSlice.actions;
+export default authSlice.reducer;
